b944cbd47e16d433bf43e6973acd5a912b282d64
RedFace <redio.development@gmail.com>
2022-12-08 16:41:48 +0100

    added precomiled lib

--- build.rs
+++ build.rs
@@ -9,29 +9,29 @@ fn main() {
 
     println!("cargo:rustc-link-lib=bluetooth");
     
-    let src = [
-        "src/bluetooth.c",
-        "src/command.c",
-        "src/connect.c",
-        "src/interface.c",
-        "src/process.c",
-        "src/state.c",
-        "src/thread.c",
-        "src/util.c"
-    ];
-    let mut builder = cc::Build::new();
-    let cc = env::var("CC_armv5te_unknown_linux_gnueabi");
-    let builder = if let Ok(cc) = cc {
-        eprintln!("Using arm-linux CC");
-        builder.compiler(cc).target("armv5te-linux-gnueabi")
-    } else {
-        eprintln!("Falling back to system CC");
-        &mut builder
-    };
-    builder
-        .files(src.iter())
-        .include("include")
-        //.flag("-lbluetooth")
-        .flag("-Wno-unused-parameter")
-        .compile("cwiid");
+    // let src = [
+    //     "src/bluetooth.c",
+    //     "src/command.c",
+    //     "src/connect.c",
+    //     "src/interface.c",
+    //     "src/process.c",
+    //     "src/state.c",
+    //     "src/thread.c",
+    //     "src/util.c"
+    // ];
+    // let mut builder = cc::Build::new();
+    // let cc = env::var("CC_armv5te_unknown_linux_gnueabi");
+    // let builder = if let Ok(cc) = cc {
+    //     eprintln!("Using arm-linux CC");
+    //     builder.compiler(cc).target("armv5te-linux-gnueabi")
+    // } else {
+    //     eprintln!("Falling back to system CC");
+    //     &mut builder
+    // };
+    // builder
+    //     .files(src.iter())
+    //     .include("include")
+    //     //.flag("-lbluetooth")
+    //     .flag("-Wno-unused-parameter")
+    //     .compile("cwiid");
 }
